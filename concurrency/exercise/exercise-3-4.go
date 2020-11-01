1b82ee536d309eb2f335ca660fe188ad629b41db
Fajar Andriansyah <fajar@myboost.id>
2020-11-01 14:20:21 +0700

    race condition using mutext: no yet

--- concurrency/exercise/exercise-3-4.go
+++ concurrency/exercise/exercise-3-4.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
 	"sync"
+	"fmt"
 )
 
 var waitGroup sync.WaitGroup
-var counter int
 
 const total = 10
 
 func foo() {
+	var counter int
+	var mutex sync.Mutex
 	for i := 0; i < total; i++ {
-		counter += i
-		runtime.Gosched()
 		go func() {
+			mutex.Lock()
+			counter += i
+			//runtime.Gosched()
 			fmt.Println("Inside Foo loop :", counter, " -> i:", i)
+			mutex.Unlock()
+			waitGroup.Done()
 		}()
-		waitGroup.Done()
 	}
 	fmt.Println("counter 1:", counter)
 }
 
 func main() {
 	waitGroup.Add(total)
-	go foo()
+	foo()
 	waitGroup.Wait()
 }
